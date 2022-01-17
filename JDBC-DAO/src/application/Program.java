7e320ac39a6f91c7a4cd717afd5209979c323870
diego.cruz <diego.cruz@accountfy.com>
2022-01-17 08:03:59 -0300

    findAll

--- JDBC-DAO/src/application/Program.java
+++ JDBC-DAO/src/application/Program.java
@@ -21,7 +21,14 @@ public class Program {
 		List<Seller> list = sellerDao.findByDepartment(department);
 
 		for (Seller obj : list) {
-			System.out.println(obj+"\n");
+			System.out.println(obj);
+		}
+		
+		System.out.println("\n=== Test 3: Seller findAll ===");
+		list = sellerDao.findAll();
+
+		for (Seller obj : list) {
+			System.out.println(obj);
 		}
 
 	}
